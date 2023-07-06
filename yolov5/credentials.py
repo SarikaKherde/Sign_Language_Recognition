ae49eb5e038afe46a15ee19b4f3659a40e361c8b
Sarika Kherde <98403085+sarika180801@users.noreply.github.com>
2023-07-06 22:00:51 +0530

    File Uploded

---
+++ yolov5/credentials.py
@@ -0,0 +1,11 @@
+
+host = 'localhost'
+user = 'root'
+password = 'Sarika@1881'
+database = 'Sign'
+# import mysql.connector
+# X = mysql.connector.connect(h,user = 'root',password = '',database = "sign")
+# if X:
+#     print("Success!")
+# C = X.cursor()
+# X.commit()
